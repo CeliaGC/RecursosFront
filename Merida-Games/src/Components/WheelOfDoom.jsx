60ed2f73395b79071e8d8b9b454621b36dd9d351
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-08-15 20:18:01 +0200

    add-selector-function-with-handleClik

--- Merida-Games/src/Components/WheelOfDoom.jsx
+++ Merida-Games/src/Components/WheelOfDoom.jsx
@@ -1,37 +1,44 @@
 import "../Style/WheelOfDoom.css"
+import React, { useState } from 'react';
 
 function WheelOfDoom() {
-
+    const [clicked, setClicked] = useState(false);
     const arrayNames = ["Anthony", "Brayan", "Antonio", "Rodrigo", "Rebeca", "Christian", "Raúl", "Yo", "Juan", "Miguel", "Isabel", "Celia", "Javi", "Laura A Secas", "Slava", "Fran", "Celia Prieto", "Gemma", "Ninguno jaja"];
-const paragraph = document.createElement('h3');
+    const paragraph = document.createElement('h3');
 
 
-function selector(arrayNames) {
-    document.getElementById("button").disabled = true;
-    const globo = document.createElement('div');
+    const handleClik = () => {
+        
+        selector(arrayNames) 
+        setClicked = true
+    }
 
-    globo.className = 'div-result'
-    const body = document.getElementsByTagName('body');
-    document.body.appendChild(globo);
+    function selector(arrayNames){
+        
+        const globo = document.createElement('div');
+
+        globo.className = 'div-result'
+        const body = document.getElementsByTagName('body');
+        document.body.appendChild(globo);
 
 
+        let result = Math.floor(Math.random() * arrayNames.length);
+        let afortunado = arrayNames[result];
+        console.log(arrayNames[result]);
+        paragraph.innerText = 'El Brishante afortunado esssss ' + afortunado;
+        globo.appendChild(paragraph);
 
-    // let result = Math.random() * arrayNames.length;
-    let result = Math.floor(Math.random() * arrayNames.length);
-    let afortunado = arrayNames[result];
-    // console.log(result);
-    console.log(arrayNames[result]);
-    paragraph.innerText = 'El Brishante afortunado esssss ' + afortunado;
-    globo.appendChild(paragraph);
-};
+    }
+
 
     return(
 
     <>
     <main id="background">
-        <button id="button" onclick="selector(arrayNames)">
+        <button id="button" onClick={handleClik}>
             Start
         </button>
+        
     </main>
     </>
     )
