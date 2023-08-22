afdbb6fe16e2dda8b4262cd9a7d536b565e4601d
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-08-22 10:21:10 +0200

    added-comments

--- Merida-Games/src/App.jsx
+++ Merida-Games/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { WheelOfDoom } from './Components/WheelOfDoom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
