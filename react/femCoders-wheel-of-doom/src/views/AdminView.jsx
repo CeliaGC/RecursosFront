aa39fed250089ec1bc613815baa4b473e2b43d49
unknown <garciacastillacelia@gmail.com>
2024-02-08 11:31:15 +0100

    add-comments-to-examples

--- react/femCoders-wheel-of-doom/src/views/AdminView.jsx
+++ react/femCoders-wheel-of-doom/src/views/AdminView.jsx
@@ -3,9 +3,18 @@ import { useState, useEffect } from "react";
 
 function AdminView(){
 
+//Vamos a renderizar un saludo distinto según la hora del día
+
+//Tenemos esta variable inicializada en un string vacío
+
     const [timeOfDay, setTimeOfDay] = useState('');
+
+    //Esta función actualiza el string con un texto distinto según la hora del día. Está envuelto en un useEffect
+    //porque necesitamos que se ejecute después de montarse el componente. Si no estuviera envuelta en él, se ejecutaría
+    //sólo al montarse el componente con la hora de ese momento y el saludo no se actualizaría.
       
     useEffect(() => {
+
       const currentTime = new Date().getHours();
       if (currentTime < 12) {
         setTimeOfDay('morning');
@@ -14,6 +23,7 @@ function AdminView(){
       } else {
         setTimeOfDay('evening');
       }
+
     }, []);
 
     return(
@@ -23,9 +33,15 @@ function AdminView(){
                 rightButton={'Sorteo'} rightButtonClick={'/sorteo'}/>
         <h1>Hi Admin</h1>
 
-        <h1>Good {timeOfDay}!</h1>
+      <h1>Good {timeOfDay}!</h1> 
+
+{/* Ahora se renderiza el saludo según la hora de día mediante un condicional lógico cortocircuitado. Si se cumple
+la condición anterior a &&, se ejecuta el elemento siguiente.
+Si timeOfDay es igual a 'morning' el saludo será Start your day right!. Y así sucesivamente conforme pasa el día y según
+la hora se actualiza el valor de timeOfDay en la función currenTime */}
+
       {timeOfDay === 'morning' && <p>Start your day right!</p>}
-      {timeOfDay === 'afternoon' && <p>Keep up the good work!</p>}
+      {timeOfDay === 'afternoon' && <p>Keep up the good work!</p>} 
       {timeOfDay === 'evening' && <p>Time to relax!</p>}
 
         </>
--- react/femCoders-wheel-of-doom/src/views/LoginView.jsx
+++ react/femCoders-wheel-of-doom/src/views/LoginView.jsx
@@ -3,25 +3,79 @@ import React, { useState } from 'react';
 
 function LoginView(){
 
+    //Vamos a hacer que cambie el renderizado según se haya hecho o no login (el login es simulado)
+
+    //Hemos inicializado esta variable con useState y un estado booleano false, queremos que cuando cambie a true
+    //(en señal de que alguien se ha logueado) cambie también el renderizado. El texto del botón es Log y el saludo es
+    //"Please log in" cuando abrimos la página. Cuando cliquemos, cambiarán a "Lets start" y "Welcome back".
+
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    //Esta función se ejecuta al hacer click y es la que cambia el estado de isLoggedIn al estado contrario al actual
     const toggleLogin = () => {
         setIsLoggedIn(!isLoggedIn);
       };
 
-return(
-    <div style={{paddingTop:"200px"}}>
-        
-        <Link to={"/home"}>
-            <button onClick={toggleLogin}>
-                <h1>{isLoggedIn ? 'Log Out' : 'Log In'}</h1>
-            </button>
-        </Link>
-        
 
-      {isLoggedIn ? <h3>Welcome back!</h3> : <h3>Please log in.</h3>}
-    </div>
-)
+      return(
 
+        <div style={{paddingTop:"200px"}}>
+                
+               
+                    <button onClick={toggleLogin}>
+
+    {/* Este es un operador ternario. Evalúa si una condición se cumple. En este caso evalua si isLoggedIn se cumple,
+    es decir,si es true o false. Si se cumple, renderiza en el h1 el primer elemento después del interrogante. Si 
+    no se cumple, renderiza el elemento siguiente a los dos puntos (que son como el else) */}
+
+                            <h1>{isLoggedIn ? 'Lets start' : 'Log In'}</h1>
+                        </button>
+
+                    {isLoggedIn ? <h3>Welcome back!</h3> : <h3>Please log in</h3>}
+            
+                 
+                </div>
+
+
+      )
 }
-export default LoginView
+
+//Este sería el renderizado condicional sin operador ternario, con un condicional if else, que requiere un return para
+//cada renderizado distinto.
+
+    //   if(isLoggedIn){
+
+    //     return(
+    //         <div style={{paddingTop:"200px"}}>
+                
+               
+    //                 <button onClick={toggleLogin}>
+    //                     <h1>Let's start</h1>
+    //                 </button>
+                
+    //             <h3>Welcome back!</h3>
+        
+             
+    //         </div>
+    //     )
+    //   } else {
+    //     return (
+    //         <div style={{paddingTop:"200px"}}>
+    //         <button onClick={toggleLogin}>
+                       
+    //                     <h1>Log in</h1>
+    //                 </button>
+    //                  <h3>Please log in.</h3>
+            
+    //         </div>
+    //     )
+    //   }
+
+
+
+
+
+export default LoginView 
+
+
+
