88721336d1ef5f4d2f2b633a5d114263ec477d29
unknown <garciacastillacelia@gmail.com>
2024-02-16 10:06:22 +0100

    fix-isLogged-typo

--- react/props-example/src/dinamic-props/ChildComponent.jsx
+++ react/props-example/src/dinamic-props/ChildComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 //Este componente va a renderizar unos datos de usuario o una petición de login dependiendo del valor de user, que serán 
 //los datos de un usuario logueado, y de isLoged, que es un estado booleano true si hay usuario registrado
- const ChildComponent = ({ user, isLoged}) => {
+ const ChildComponent = ({ user, isLogged}) => {
   return (
     <div>
-      {isLoged ? (
+      {isLogged ? (
         <div>
           <h1>User Profile</h1>
           <p>Name: {user.name}</p>
