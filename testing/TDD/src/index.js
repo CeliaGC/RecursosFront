2b17591fb1da9a5018f9d90902f7e1be34022408
unknown <garciacastillacelia@gmail.com>
2024-04-25 13:20:09 +0200

    add-snapshot

--- testing/TDD/src/index.js
+++ testing/TDD/src/index.js
@@ -28,3 +28,70 @@ function fizzBuzz(number) {
 }
 
 export default fizzBuzz;
+
+
+
+// function fizzBuzz(number){
+
+// let result;
+// switch(number){
+    
+//     case number.length > 1 :
+
+//     result = []
+//         number.forEach(n => {
+           
+
+//             if(n % 15 === 0){
+
+//                 result.push('fizzBuzz')     }
+        
+            
+//             if(n % 3 === 0 ){
+                
+//                 result.push('fizz')
+            
+//             }
+        
+//             if(n % 5 === 0){
+
+//                 result.push('buzz')
+                
+//             }
+
+//             if(n % 5 || 3|| 15 === 0)
+
+//                 result.push(n)
+
+//         //  number.push(n)   
+            
+//         });
+            
+    
+//     break;
+
+//     case number == 1:
+
+//     result.toString()
+
+//     if(number % 15 === 0){
+
+//         result = 'fizzBuzz'
+//     }
+
+    
+//     if(number % 3 === 0 ){
+        
+//         result = 'fizz'
+//     }
+
+//     if(number % 5 === 0){
+
+//         result = 'buzz'
+//     }
+
+// }
+
+//     return result
+        
+// }
--- testing/TDD/test/index.test.js
+++ testing/TDD/test/index.test.js
@@ -31,7 +31,57 @@ test('devuelve buzz si el número es divisible entre 5', ()=> {
     expect(result).toBe("fizzBuzz");
 });
 
+
+test('devuelve números del 1 al 15 con las condiciones anteriores',()=>{
+
+    let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    
+
+    let result = fizzBuzz(numbers);
+    
+
+    expect(result).toEqual([ 
+        1,
+        2,
+        "fizz",
+        4,
+        "buzz",
+        "fizz",
+        7,
+        8,
+        "fizz",
+        "buzz",
+        11,
+        "fizz",
+        13,
+        14,
+        "fizzBuzz"])
+
+        // expect(result).toEqual([ 1,
+        //     2,
+        //     "fizz",
+        //     4,
+        //     "buzz",
+        //     "fizz",
+        //     7,
+        //     8,
+        //     "fizz",
+        //     "buzz",
+        //     11,
+        //     "fizz",
+        //     13,
+        //     14,
+        //     "fizzBuzz"])
+});
+
+
 test('devuelve números del 1 al 100 con las condiciones anteriores',()=>{
+
+    // let numbers = [];
+    // for (let index = 1; index < 100; index++) {
+    //     numbers.push(index);
+        
+    // }
 
     let numbers = Array.from({length: 100}, (_, i) => i + 1);
   
@@ -40,8 +90,12 @@ test('devuelve números del 1 al 100 con las condiciones anteriores',()=>{
     
 
     expect(result).toMatchSnapshot();
+    
 
-})
+});
+
+
+
 
 
 
